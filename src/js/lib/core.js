da7bc7e4e6c0070e86861e8a4fc318e52b851826
Vlad <qwerasvlad@gmail.com>
2021-10-19 15:16:34 +0300

    add $ = selector(All) and (hide, show and torggle for elements

---
+++ src/js/lib/core.js
@@ -0,0 +1,44 @@
+// (() => {
+//     const $ = function (selector) {
+//         const elements = document.querySelectorAll(selector);
+//         const obj = {};
+
+//         obj.hide = () => {
+//             elements.forEach(elem => {
+//                 elem.style.display = 'none';
+//             });
+//             return obj;
+//         };
+
+//         obj.show = () => {
+//             elements.forEach(elem => {
+//                 elem.style.display = '';
+//             });
+//             return obj;
+//         };
+
+//         return obj;
+
+//     };
+
+//     window.$ = $;
+// })();
+
+const $ = function (selector) {
+    return new $.prototype.init(selector);
+};
+
+$.prototype.init = function(selector) {
+    if (!selector) {
+        return this; // {}
+    }
+    Object.assign(this, document.querySelectorAll(selector));
+    this.length = document.querySelectorAll(selector).length;
+    return this;
+};
+
+$.prototype.init.prototype = $.prototype;
+
+window.$ = $;
+
+export default $;
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,4 @@
-console.log('Hello!');
+import './lib/lib';
+
+
+$('.active').toggle();
